7a05f7c9c35fe6d681ea6dab2dcc6c6187a763d5
waodng <454802367@qq.com>
2024-06-11 11:52:19 +0800

    init

---
+++ netcore/Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Demo;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str="";
+        Console.WriteLine("Hello, World!");
+        str+="你1好";
+        System.Console.WriteLine(str);
+        System.Console.WriteLine("这是一个中文乱码测试");
+    }
+}
